a0e59fcad1a63ec51e4d92ec3a3ee194bd42935b
agomeza21 <gomezandresa03@gmail.com>
2026-03-11 16:16:11 +0100

    primeros ejercicios del modulo 01

---
+++ module01/ex0/ft_garden_intro.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+
+if __name__ == "__main__":
+    plant = "Rose"
+    height = "25cm"
+    age = "30 days"
+    print("=== Welcome to My Garden ===")
+    print("Plant:", plant)
+    print("Height:", height)
+    print("Age:", age)
+    print(" ")
+    print("=== End of Program ===")
---
+++ module01/ex2/ft_plant_growth.py
@@ -0,0 +1,26 @@
+class Plant:
+    def __init__(self, name: str, height: int, plant_age: int):
+        self.name = name
+        self.height = height
+        self.plant_age = plant_age
+
+    def grow(self):
+        self.height += 1
+
+    def age(self):
+        self.plant_age += 1
+
+    def get_info(self):
+        print(f"{self.name}: {self.height}cm, {self.plant_age} days old")
+
+
+if __name__ == "__main__":
+    plant1 = Plant("Rose", 25, 30)
+    count = -1
+    for i in range(1, 8):
+        print(f"=== Day {i} ===")
+        plant1.get_info()
+        plant1.grow()
+        plant1.age()
+        count = count + 1
+    print(f"Growth this week: +{count}")
