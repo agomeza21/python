856fca147318eec47c26e7772930871ee71dce61
agomeza21 <gomezandresa03@gmail.com>
2026-03-11 17:22:14 +0100

    cmabiado el 1 para usar for

--- module01/ex1/ft_garden_data.py
+++ module01/ex1/ft_garden_data.py
@@ -10,9 +10,9 @@ def main() -> None:
     plant2 = Plant("Sunflower", 80, 45)
     plant3 = Plant("Cactus", 15, 120)
     print("=== Garden Plant Registry ===")
-    print(f"{plant1.name}: {plant1.height}cm, {plant1.age} days old")
-    print(f"{plant2.name}: {plant2.height}cm, {plant2.age} days old")
-    print(f"{plant3.name}: {plant3.height}cm, {plant3.age} days old")
+    plants = [plant1, plant2, plant3]
+    for plant in plants:
+        print(f"{plant.name}: {plant.height}cm, {plant.age} days old")
 
 
 if __name__ == "__main__":
