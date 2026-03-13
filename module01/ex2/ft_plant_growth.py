9bf503a6cf6a07435540d2bcb1dc5dbd628ddcc6
agomeza21 <gomezandresa03@gmail.com>
2026-03-13 13:12:24 +0100

    corregidos type hints

--- module01/ex2/ft_plant_growth.py
+++ module01/ex2/ft_plant_growth.py
@@ -1,16 +1,16 @@
 class Plant:
-    def __init__(self, name: str, height: int, plant_age: int):
+    def __init__(self, name: str, height: int, plant_age: int) -> None:
         self.name = name
         self.height = height
         self.plant_age = plant_age
 
-    def grow(self):
+    def grow(self) -> None:
         self.height += 1
 
-    def age(self):
+    def age(self) -> None:
         self.plant_age += 1
 
-    def get_info(self):
+    def get_info(self) -> None:
         print(f"{self.name}: {self.height}cm, {self.plant_age} days old")
 
 
