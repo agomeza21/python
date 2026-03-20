e915899e6bc48909d5ec316c66bfab6c80629306
agomeza21 <gomezandresa03@gmail.com>
2026-03-20 11:47:02 +0100

    ejercicio 2 terminado

--- module02/ex2/ft_different_errors.py
+++ module02/ex2/ft_different_errors.py
@@ -1,49 +1,27 @@
-def garden_operations() -> None:
-    print("Testing ValueError...")
-    try:
-        temperature = "abc"
-        temp = int(temperature)
-    except ValueError as t:
-        print(f"Caught {type(t).__name__}: {t}")
-    print("")
-    print("Testing ZeroDivisionError...")
-    try:
-        plants = 100
-        sectors = 0
-        sector_plants = plants / sectors
-    except ZeroDivisionError as d:
-        print(f"Caught {type(d).__name__}: {d}")
-    print("")
-    print("Testing FileNotFoundError...")
-    try:
-        open("text.txt")
-    except FileNotFoundError as f:
-        print(f"Caught {type(f).__name__}: {f}")
-    print("")
-    print("Testing KeyError...")
-    try:
-        flowers = {"tulip": "blue", "rose": "red"}
-        flowers["sunflower"]
-    except KeyError as e:
-        print(f"Caught {type(e).__name__}: {e}")
-    print("")
-    print("Testing multiple errors toguether...")
-    try:
-        temperature = "abc"
-        temp = int(temperature)
-        plants = 100
-        sectors = 0
-        sector_plants = plants / sectors
-    except (ValueError, ZeroDivisionError):
-        print("Caught an error, but program continues!")
+def garden_operations(operation_number: int) -> None:
+    if operation_number == 0:
+        int("abc")
+    elif operation_number == 1:
+        _ = 100 / 0
+    elif operation_number == 2:
+        open("file.txt")
+    elif operation_number == 3:
+        _ = "temperature" + 25
+
 
 def test_error_types() -> None:
     print("=== Garden Error Types Demo ===")
-    print("")
-    garden_operations()
+    for i in range(5):
+        print(f"Testing operation {i}...")
+        try:
+            garden_operations(i)
+            print("Operation completed successfully")
+        except (ValueError, ZeroDivisionError, FileNotFoundError,
+                TypeError) as e:
+            print(f"Caught {type(e).__name__}: {e}")
     print("")
     print("All error types tested successfully!")
 
 
-if __name__=="__main__":
+if __name__ == "__main__":
     test_error_types()
