8a9be6bbdaf6c3e44c493035be7a6947524192b4
agomeza21 <gomezandresa03@gmail.com>
2026-03-24 13:29:20 +0100

    ejercicio 1 del modulo03 terminado

--- module03/ex1/ft_score_analytics.py
+++ module03/ex1/ft_score_analytics.py
@@ -4,14 +4,36 @@ import sys
 def create_list() -> list:
     score = []
     for i in range(1, len(sys.argv)):
-        score.append(sys.argv[i])
+        try:
+            score.append(int(sys.argv[i]))
+        except ValueError:
+            print(f"Invalid parameter: '{sys.argv[i]}'")
     return score
 
 
 def main() -> None:
     print("=== Player Score Analytics ===")
+    if len(sys.argv) == 1:
+        print("No scores provided.", end="")
+        print(" Usage: python3 ft_score_analytics.py", end="")
+        print(" <score1> <score2> ...")
+        return
     score = create_list()
+    if len(score) == 0:
+        print("No scores provided.", end="")
+        print("Usage: python3 ft_score_analytics.py", end="")
+        print(" <score1> <score2> ...")
+        return
     print(f"Scores processed: {score}")
+    print(f"Total players: {len(score)}")
+    total = sum(score)
+    print(f"Total score: {total}")
+    print(f"Average score: {total / len(score)}")
+    max_num = max(score)
+    print(f"High score: {max_num}")
+    min_num = min(score)
+    print(f"Low score: {min_num}")
+    print(f"Score range: {max_num - min_num}")
 
 
 if __name__ == "__main__":
