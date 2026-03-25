c26e868a0d7aa78b7085b6074845b8d7df4050eb
agomeza21 <gomezandresa03@gmail.com>
2026-03-25 15:37:20 +0100

    ejercicio 1 modulo4 terminado

---
+++ module04/ex0/ft_ancient_text.py
@@ -0,0 +1,21 @@
+def main() -> None:
+    print("=== CYBER ARCHIVES - DATA RECOVERY SYSTEM ===")
+    print("")
+    try:
+        file = open("ancient_fragment.txt")
+        print("Accessing Storage Vault: ancient_fragment.txt")
+        print("Connection established...")
+    except FileNotFoundError:
+        print("Storage vault not found.")
+        print("")
+        print("Not able to start data recovery.")
+        return
+    print("")
+    print("RECOVERED DATA:")
+    print(file.read())
+    print("")
+    print("Data recovery complete. Storage unit disconnected.")
+
+
+if __name__ == "__main__":
+    main()
