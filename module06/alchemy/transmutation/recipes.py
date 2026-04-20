e378c33eefcff5f9195aba87978d9cb785be52db
agomeza21 <gomezandresa03@gmail.com>
2026-04-20 12:31:22 +0200

    continuará...

--- module06/alchemy/transmutation/recipes.py
+++ module06/alchemy/transmutation/recipes.py
@@ -1,4 +1,8 @@
 '#alchemy/transmutation/recipes.py'
 
+from ..elements import create_air
+from ..potions import strength_potion
+from ..potions import elements
+
 def lead_to_gold() -> str:
-    return 
+    return (f"Recipe transmuting Lead to Gold: brew ’[{create_air()}]’ and ’[{strength_potion()}]’ mixed with ’[{create_fire()}]’")
