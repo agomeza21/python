de158b4872388d673827f2aa277ebe41ce07a962
agomeza21 <gomezandresa03@gmail.com>
2026-04-23 17:09:51 +0200

    EMPEZADO EX1 DEL MODULO07

---
+++ module07/ex1/new_creatures.py
@@ -0,0 +1,16 @@
+from ex0.creature import Creature
+
+
+class Sproutling(Creature, HealCapability):
+
+
+
+class Bloomelle(Creature, HealCapability):
+
+
+
+class Shiftling(Creature, TransformCapability):
+
+
+
+class Morphagon(Creature, TransformCapability):
