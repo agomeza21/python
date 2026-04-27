34fd0a641be177cf5ac130ba77a6c2cfd99bb50a
agomeza21 <gomezandresa03@gmail.com>
2026-04-27 13:46:18 +0200

    corregidas type annotations

--- module07/ex1/new_factories.py
+++ module07/ex1/new_factories.py
@@ -1,19 +1,20 @@
 '#ex1/new_factories.py'
 from ex0.factories import CreatureFactory
 from .new_creatures import Sproutling, Bloomelle, Shiftling, Morphagon
+from .new_creatures import Creature
 
 
 class HealingCreatureFactory(CreatureFactory):
-    def create_base(self):
+    def create_base(self) -> Creature:
         return Sproutling()
 
-    def create_evolved(self):
+    def create_evolved(self) -> Creature:
         return Bloomelle()
 
 
 class TransformCreatureFactory(CreatureFactory):
-    def create_base(self):
+    def create_base(self) -> Creature:
         return Shiftling()
 
-    def create_evolved(self):
+    def create_evolved(self) -> Creature:
         return Morphagon()
